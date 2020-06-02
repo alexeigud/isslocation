89daa0e86ce3afbb70cddcf7c5e5458f97687bd1
Alexei Goudzenko <alexgud300@gmail.com>
2020-06-02 11:25:34 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            location: {
+                message: null,
+                timestamp: null,
+                iss_position: {
+                    latitude: null, 
+                    longitude: null
+                }
+            }
+        }
+        this.interval = null;
+    }
+
+    async getData() {
+        const url = "http://api.open-notify.org/iss-now.json";
+        const response = await fetch(url);
+        const location = await response.json();
+
+        this.setState({ location });
+    }
+
+
+    async componentDidMount() {
+        this.interval = setInterval(async () => await this.getData(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        const { location } = this.state;
+            return (
+                <div>
+                   {location.message === 'success' &&
+                    <p>
+                        Latitude: {location.iss_position.latitude} 
+                        <br/>
+                        Longitude: {location.iss_position.longitude}
+                        <br/>
+                        Timestamp: {location.timestamp}
+                    </p>
+                   }
+                </div>
+            )     
+        }
 }
 
-export default App;
+export default App
