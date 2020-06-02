e85f247b0b3abab66369cd3268c22fa2ed8a8b12
Alexei Goudzenko <alexgud300@gmail.com>
2020-06-02 11:26:36 -0400

    Updates

---
+++ precache-manifest.9c72f495de191f0a37d1efdf70d09595.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8990cfee10f4fc14aed823abb4524d50",
+    "url": "/isslocation/index.html"
+  },
+  {
+    "revision": "d828be65ac6cb8edc4c0",
+    "url": "/isslocation/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b9504df38d08c6aeb4ab",
+    "url": "/isslocation/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d828be65ac6cb8edc4c0",
+    "url": "/isslocation/static/js/2.7c83020b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/isslocation/static/js/2.7c83020b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9504df38d08c6aeb4ab",
+    "url": "/isslocation/static/js/main.8e7e07cb.chunk.js"
+  },
+  {
+    "revision": "9646f9bf540066adf70e",
+    "url": "/isslocation/static/js/runtime-main.7b5d170d.js"
+  }
+]);
